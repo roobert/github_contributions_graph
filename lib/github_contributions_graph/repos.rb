a1def6069ad258ee4e2567cd338a489b732676af
Rob Wilson <roobert@gmail.com>
2016-01-03 15:24:38 +0000

    remove unnecessary class

---
+++ lib/github_contributions_graph/repos.rb
@@ -0,0 +1,86 @@
+#!/usr/bin/env ruby
+#encoding=utf-8
+
+require 'forwardable'
+
+module GithubContributionsGraph
+  class Repos
+    extend Forwardable
+
+    def initialize
+      @repos = load_repos(config)
+    end
+
+    def_delegators :@repos, :each, :[]
+
+    def config
+      YAML.load_file('config.yml')['repos']
+    end
+
+    def load_repos(config)
+      config.keys.each_with_index.map do |repo, index|
+        Repo.new(repo, Color.palette(index), config[repo])
+      end
+    end
+  end
+
+  class Repo
+    attr_accessor :name, :days, :palette, :config
+
+    def initialize(name, palette, config)
+      @name    = name
+      @palette = palette
+      @days    = Days.new(name, palette)
+      @config  = config
+    end
+
+    def to_s
+      "repo: #{@name}"
+    end
+  end
+
+  class Days
+    extend Forwardable
+
+    attr_accessor :days
+
+    def initialize(name, palette)
+      @days = []
+      load_days(name, palette)
+    end
+
+    def_delegators :@days, :each
+
+    def load_days(name, palette)
+      store(name).each do |date, commits|
+        @days.push Day.new(date, commits, palette)
+      end
+    end
+
+    def store(name)
+      @store ||= YAML.load_file("#{name}.yaml")
+    end
+  end
+
+  class Day
+    attr_accessor :date, :commits
+
+    def initialize(date, commits, palette)
+      @date    = date
+      @commits = commits
+      @palette = palette
+    end
+
+    def color
+      @color ||= Color.contributions(commits, @palette)
+    end
+
+    def ==(other)
+      commits == other.commits
+    end
+
+    def to_s
+      "#{date}: #{commits.ljust(3)} - ##{color.hex}"
+    end
+  end
+end
