ca4f7d4091801fca644998c2aea067ad70dcba64
Rob Wilson <roobert@gmail.com>
2016-01-03 15:24:56 +0000

    rename comparer to repos

--- lib/github_contributions_graph.rb
+++ lib/github_contributions_graph.rb
@@ -4,5 +4,5 @@
 require 'github_contributions_graph/database'
 require 'github_contributions_graph/remote'
 require 'github_contributions_graph/remote/login'
-require 'github_contributions_graph/comparer'
+require 'github_contributions_graph/repos.rb'
 require 'github_contributions_graph/color'
